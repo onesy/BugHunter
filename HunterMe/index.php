26f116d37c0951595fef15e164fed8a719347f14
onesy <leentingOne@gmail.com>
2013-03-19 16:46:11 +0800

    重构尚未完成，数据还要调整

--- HunterMe/index.php
+++ HunterMe/index.php
@@ -1,7 +1,9 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * 需要做的事情
+ * 1.配置文件
+ * 2.加载类加载器
  */
+require_once 'RootCfg.php';//加载配置文件
 ?>
