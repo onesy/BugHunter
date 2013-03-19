ec273d46b55e59c3c7ae1985c5d121825d2fe11f
onesy <leentingOne@gmail.com>
2013-03-19 11:31:43 +0800

    加载器建立

---
+++ RootCfg.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$CfgConfig=array();
+/**
+ * 指定其他配置文件的位置
+ */
+define("FRAMEWORK_PATH", 'BH_Framework');
+
+?>
--- index.php
+++ index.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php
+/**
+ * 需要做的事情
+ * 1.配置文件
+ * 2.加载类加载器
+ */
+?>
